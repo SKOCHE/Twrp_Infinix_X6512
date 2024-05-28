5080314f2cd011bbc3adf18dfb78f3e7f6d4d464
SKOCHE <101643020+SKOCHE@users.noreply.github.com>
2024-05-28 14:33:01 +0700

    Update twrp_X6512.mk

--- twrp_X6512.mk
+++ twrp_X6512.mk
@@ -4,9 +4,9 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
@@ -14,13 +14,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # Virtual A/B OTA
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
-# Inherit some common twrp stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
-
 # Inherit from X6512 device
 $(call inherit-product, device/infinix/X6512/device.mk)
 
-# Device identifier. This must come after all inclusions														
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := X6512
 PRODUCT_NAME := twrp_X6512
 PRODUCT_BRAND := infinix
